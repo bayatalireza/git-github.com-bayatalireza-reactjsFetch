04b675b266df4ef06f4e64beebdc92eb57669fc1
alireza <bayatalireza86@gmail.com>
2024-09-15 16:32:39 +0330

    add new hook in Hooks files

--- src/Component/Hooks/useFetch.js
+++ src/Component/Hooks/useFetch.js
@@ -1,38 +1,38 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
-// function useFetch(url){
-//     const [data, setData] = useState(null)
-//     const [isLoading, setIsLoading] = useState(false)
-//     const [error, setError] = useState(null)
+function useFetch(url){
+    const [data, setData] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
     
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             setIsLoading(true)
-//             try {
-//                 const response = await fetch(url)
-//                 // console.log(response);
-//                 if (!response.ok) {
-//                    throw new Error(response.statusText);
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true)
+            try {
+                const response = await fetch(url)
+                // console.log(response);
+                if (!response.ok) {
+                   throw new Error(response.statusText);
                     
-//                 }
+                }
                 
-//                 const json = await response.json()
+                const json = await response.json()
                 
-//                 setIsLoading(false)
+                setIsLoading(false)
                 
-//                 setData(json)
-//                 setError(null) 
-//             } catch (err) {
-//                 setIsLoading(false)
-//                setError(err.message) 
+                setData(json)
+                setError(null) 
+            } catch (err) {
+                setIsLoading(false)
+               setError(err.message) 
 
                
-//             }
+            }
             
 
-//         }
-//         fetchData()
-//     },[url])
-//     return {data, isLoading,error}
-// }
-// export default useFetch;
+        }
+        fetchData()
+    },[url])
+    return {data, isLoading,error}
+}
+export default useFetch;
--- src/Component/Trips/trips.js
+++ src/Component/Trips/trips.js
@@ -1,97 +1,50 @@
-// import { useState } from "react";
-// import "./trips.css";
-// import useFetch from "../Hooks/useFetch";
-
-// import { useState } from "react"
-
-// function Trips() {
-//   const [url, setUrl] = useState("http://localhost:3000/trips21");
-//     const{data: trips, isLoading, error} = useFetch(url)
-
-//   return (
-//     <>
-//       <h1 className="trips">Trips List</h1>
-//       {isLoading &&
-//       <div>
-//         <h2>Is Loading...</h2>
-//       </div>
-//       }
-//       {error &&
-//       <div>
-//         <h2>{error}</h2>
-//       </div>
-//       }
-//       <ul>
-//         {trips && trips.map((trip) => (
-//           <li key={trip.id} className="li">
-//             <h3>
-//               <span>{trip.id}</span> - {trip.title}
-//             </h3>
-//             <p>{trip.price}</p>
-//           </li>
-//         ))}
-//       </ul>
-//       <div className="button">
-//         <button onClick={() => setUrl("http://localhost:3000/trips3?loc=iran")}>
-//           Trips in Iran
-//         </button>
-//         <button onClick={() => setUrl("http://localhost:3000/trips3")}>
-//           All Trips
-//         </button>
-//         <button
-//           onClick={() => setUrl("http://localhost:3000/trips3?loc=turkey")}
-//         >
-//           turkey Trips
-//         </button>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Trips;
-
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 import "./trips.css";
+import useFetch from "../Hooks/useFetch";
 
 
+function Trips() {
+  const [url, setUrl] = useState("http://localhost:3000/trips21");
+    const{data: trips, isLoading, error} = useFetch(url)
 
-function Trips(){
-  const [trips, setTrips] = useState([])
-  const [url, setUrl] = useState("http://localhost:3000/trips3")
-  
-    const fetchTrips = useCallback(async()=>{
-      const response = await fetch(url)
-      const json = await response.json()
-      setTrips(json)
-    
-  },[url])
-  useEffect(()=>{
-    fetchTrips()
-  },[fetchTrips])
-
-  return(
-    <div>
-      <h1>All Trips</h1>
-    
-      <ul className="trips">
-        {
-          trips.map((trip)=>(
-            <li key={trip.id} className="li">
-              <h2>{trip.title}</h2>
-              <p>{trip.price}</p>
-            </li>
-          ))
-        }
+  return (
+    <>
+      <h1 className="trips">Trips List</h1>
+      {isLoading &&
+      <div>
+        <h2>Is Loading...</h2>
+      </div>
+      }
+      {error &&
+      <div>
+        <h2>{error}</h2>
+      </div>
+      }
+      <ul>
+        {trips && trips.map((trip) => (
+          <li key={trip.id} className="li">
+            <h3>
+              <span>{trip.id}</span> - {trip.title}
+            </h3>
+            <p>{trip.price}</p>
+          </li>
+        ))}
       </ul>
       <div className="button">
-    <button onClick={()=>setUrl("http://localhost:3000/trips3?loc=iran")}>Trips Iranian</button>
-    <button onClick={()=>setUrl("http://localhost:3000/trips3?loc=turkey")}>Trips Turkey</button>
-    <button onClick={()=>setUrl("http://localhost:3000/trips3")}>Trips All</button>
-
+        <button onClick={() => setUrl("http://localhost:3000/trips3?loc=iran")}>
+          Trips in Iran
+        </button>
+        <button onClick={() => setUrl("http://localhost:3000/trips3")}>
+          All Trips
+        </button>
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips3?loc=turkey")}
+        >
+          turkey Trips
+        </button>
       </div>
-
-    </div>
-  )
+    </>
+  );
 }
 
 export default Trips;
